b7e7edc57ac756502203c032ae3272e8340804ea
PizzocaroSolomon <solomon.pizzocaro@polimi.it>
2022-03-10 00:25:31 +0100

    widgets position tutorial

--- tutorial_flutter/tutorial01_widgets/lib/main.dart
+++ tutorial_flutter/tutorial01_widgets/lib/main.dart
@@ -3,18 +3,59 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String myText = "Hello World";
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Color.fromARGB(50, 0, 0, 0),
-        padding: const EdgeInsets.all(36),
-        child: Text(
-          this.myText,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(backgroundColor: Colors.blue, fontSize: 20),
+    return MaterialApp(
+      title: 'Login Application',
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('posizionamento widgets')),
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 16.0),
+                  child: Row(children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(right: 8.0),
+                      child: Text('USERNAME'),
+                    ),
+                    Flexible(child: TextField()),
+                  ]),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 16.0),
+                  child: Row(children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(right: 8.0),
+                      child: Text('PASSWORD'),
+                    ),
+                    Flexible(
+                        child: TextField(
+                      obscureText: true,
+                    )),
+                  ]),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    RaisedButton(
+                      onPressed: () => {},
+                      child: Text('Login'),
+                    ),
+                  ],
+                ),
+              ]),
         ));
   }
 }
